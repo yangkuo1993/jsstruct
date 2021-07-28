a5ab588b57fc044265785361ae7c7206fb3909a3
yangkuo <kuo.yang@niu.com>
2021-07-28 14:57:36 +0800

    链表

--- linklist/linklist17.js
+++ linklist/linklist17.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-28 11:35:55
- * @LastEditTime: 2021-07-28 11:35:56
+ * @LastEditTime: 2021-07-28 13:54:08
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /jsstruct/linklist/linklist17.js
@@ -48,3 +48,75 @@ myHashSet.contains(2); // 返回 False ，（已移除）
 链接：https://leetcode-cn.com/problems/design-hashset
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+
+/**
+ * @param {ListNode} node
+ * Initialize your data structure here.
+ */
+ var MyHashSet = function() {
+    this.list = null;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    if (this.list === null) {
+        this.list = new ListNode(key);
+    } else {
+        let now = this.list;
+        while(now.next) {
+            now = now.next;
+        }
+        now.next = new ListNode(key);
+    }
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    let dummy = new ListNode(-1);
+    dummy.next = this.list;
+    let now = dummy;
+    while(now.next) {
+        if (now.next.val === key) {
+            now.next = now.next.next;
+        } else {
+            now = now.next;
+        }
+    }
+};
+
+/**
+ * Returns true if this set contains the specified element 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    let now = this.list;
+    while(now) {
+        if (now.val === key) {
+            return true;
+        }
+        now = now.next;
+    }
+    return false;
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
+
+ var obj = new MyHashSet()
+ obj.add(1)
+ obj.add(2)
+ obj.add(3)
+ obj.remove(2)
+console.log(obj.list)
