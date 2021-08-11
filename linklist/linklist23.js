5889d7a04b781061378e1b1b332fc1c453ec9f55
yangkuo <kuo.yang@niu.com>
2021-08-11 18:20:48 +0800

    linklist

---
+++ linklist/linklist23.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-11 18:12:08
+ * @LastEditTime: 2021-08-11 18:15:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /jsstruct/linklist/linklist23.js
+ */
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ */
+
+
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+ var reversePrint = function(head) {
+
+};
