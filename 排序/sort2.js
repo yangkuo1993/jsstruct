c1718ec8f6e50d9322bc46cf06710fee2d843ebb
yangkuo <kuo.yang@niu.com>
2022-01-13 18:04:02 +0800

    排序

--- 排序/sort2.js
+++ 排序/sort2.js
@@ -31,10 +31,58 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-const arr1 = [4, 8, 1, 7, 3, 2, 5, 6];
+var arr1 = [
+  3, 44, 38, 5, 47, 15, 36, 26, 27, -1, -3, -3, -44, 48, 2, 46, 4, 19, 50, 48,
+];
 console.time("冒泡排序耗时");
 console.log("bubbleSort :", bubbleSort(arr1));
 console.timeEnd("冒泡排序耗时");
+
+/// 冒泡排序改进版
+const bubbleSort2 = (arr) => {
+  var i = arr.length - 1;
+  while (i > 0) {
+    var pos = 0;
+    for (var j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        pos = j;
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+      }
+    }
+    i = pos;
+  }
+  return arr;
+};
+
+console.time("改进冒泡排序耗时");
+console.log("bubbleSort2 :", bubbleSort2(arr1));
+console.timeEnd("改进冒泡排序耗时");
+
+/// 冒泡排序改进第二版
+
+const bubbleSort3 = (arr) => {
+    let low = 0;
+    let high = arr.length - 1;
+    while(low < high) {
+        for (let i =0; i < high; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]];
+            }
+        }
+        --high;
+        for (let j = high; j > low;j--) {
+            if (arr[j] < arr[j - 1]) {
+                [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+            }
+        }
+        ++low;
+    }
+    return arr;
+}
+
+console.time("改进冒泡排序耗时3");
+console.log("bubbleSort3 :", bubbleSort3(arr1));
+console.timeEnd("改进冒泡排序耗时3");
 
 /**
  * =====================分隔=====================
@@ -52,23 +100,23 @@ console.timeEnd("冒泡排序耗时");
  *
  */
 
-function insertSort(array) {
-  for (let i = 1; i < array.length; i++) {
-    let target = i;
-    for (let j = i - 1; j >= 0; j--) {
-      if (array[target] < array[j]) {
-        [array[target], array[j]] = [array[j], array[target]];
-        target = j;
-      } else {
-        break;
-      }
-    }
-  }
-  return array;
-}
-console.time("插入排序耗时");
-console.log("insertSort :", bubbleSort(arr1));
-console.timeEnd("插入排序耗时");
+// function insertSort(array) {
+//   for (let i = 1; i < array.length; i++) {
+//     let target = i;
+//     for (let j = i - 1; j >= 0; j--) {
+//       if (array[target] < array[j]) {
+//         [array[target], array[j]] = [array[j], array[target]];
+//         target = j;
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+//   return array;
+// }
+// console.time("插入排序耗时");
+// console.log("insertSort :", bubbleSort(arr1));
+// console.timeEnd("插入排序耗时");
 
 /**
  * =====================分隔=====================
@@ -82,20 +130,19 @@ console.timeEnd("插入排序耗时");
 空间复杂度:O(1)
  */
 
+// const selectionSort = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] < array[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     [array[minIndex], array[i]] = [array[i], array[minIndex]];
+//   }
+//   return array;
+// };
 
-const selectionSort = array => {
-    for(let i =0; i < array.length; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[j] < array[minIndex] ) {
-                minIndex = j;
-            }
-        }
-        [array[minIndex], array[i]] = [array[i], array[minIndex]];
-    }
-    return array;
-}
-
-console.time("选择排序耗时");
-console.log("selectionSort :", selectionSort(arr1));
-console.timeEnd("选择排序耗时");
+// console.time("选择排序耗时");
+// console.log("selectionSort :", selectionSort(arr1));
+// console.timeEnd("选择排序耗时");
